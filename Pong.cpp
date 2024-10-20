160fc2e361e1a00a56ad6b3559fa8c3764d741e0
eduardo.zekl@gmail.com <eduardo.zekl@gmail.com>
2024-10-20 17:53:20 -0300

    opening window

--- Pong.cpp
+++ Pong.cpp
@@ -1,2 +1,77 @@
 #include <Windows.h>
+#include <tchar.h>
 
+
+static TCHAR szWindowClass[] = _T("DesktopApp");
+static TCHAR szTitle[] = _T("Game");
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASSEX wcex = { };
+
+    wcex.cbSize         = sizeof(WNDCLASSEX);
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WindowProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = NULL;
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+    if(!RegisterClassEx(&wcex)) 
+    {
+        MessageBox(NULL, _T("Error registering window"), _T("ERROR"), NULL);
+
+        return 0;
+    }
+
+    HWND hWnd = CreateWindowEx(WS_EX_OVERLAPPEDWINDOW, szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 500, 100, NULL, NULL, hInstance, NULL);
+    DWORD dw = GetLastError(); 
+    
+    if(!hWnd) 
+    {
+        MessageBox(NULL, _T("Error creating window"), _T("ERROR"), NULL);
+
+        return 0;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) 
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int) msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+    TCHAR greeting[] = _T("ABNER!");
+
+    switch (uMsg) {
+        case WM_PAINT:
+            hdc = BeginPaint(hwnd, &ps);
+            TextOut(hdc, 5, 5, greeting, _tcslen(greeting));
+            EndPaint(hwnd, &ps);
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+            break;
+    }
+
+    return 0;
+}
